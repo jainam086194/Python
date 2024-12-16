f7040a13cb19616951bf25f41071b7f262308bd1
jainam086194 <jainam51297@gmail.com>
2024-12-16 16:26:59 +0000

    First commit

---
+++ first.py
@@ -0,0 +1,21 @@
+print("Hello wolr")
+
+
+# print based on condition
+# x = 10
+# if x==10:
+#     print("casas")
+    
+# '''
+# multiline comments
+# also called function docstring
+# '''
+
+# x,y,z = "xaxsac", 293, True
+
+# # print(x,y,z)
+
+# print(type(x))
+
+
+x,y,z = ("orange", "asdasd", "cdcsdc", "cdcd")
